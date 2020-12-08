0a29204255d0c28258a2e8a416ecb88cf66274ba
Nihad Fejzic <nfejzic2@etf.unsa.ba>
2020-12-08 17:52:42 +0100

    added script Poslovnice

---
+++ seminarski.sql
@@ -0,0 +1,71 @@
+SELECT *
+FROM poslovnice;
+
+INSERT INTO poslovnice
+VALUE (DEFAULT,  
+'Apoteka Sarajevo',
+'Srajevo',
+'Trg Fra Grge Martića 2',
+'033 770-650',
+'info@apoteke-sarajevo.ba');
+
+INSERT INTO poslovnice
+VALUE (DEFAULT, 
+'Vita',
+'Srajevo',
+'Maršala Tita 44',
+'033 222-522',
+'centrala@apotekavita.com');
+
+INSERT INTO poslovnice
+VALUE (DEFAULT, 
+'Apoteke Mopharm',
+'Mostar',
+'Husnije Repca bb',
+'036 580-535',
+'mo-pharm@bih.net.ba');
+
+INSERT INTO poslovnice
+VALUE (DEFAULT, 
+'Apoteka Eurovita Pharmacia',
+'Mostar',
+'Braće Fejića 5',
+'+036 551-695',
+'info@eurovita.ba');
+
+INSERT INTO poslovnice
+VALUE (DEFAULT, 
+'Apoteke Mediflor',
+'Tuzla',
+'Univerzitetska 16',
+'035 278-333',
+'info@mediflor.ba');
+
+INSERT INTO poslovnice
+VALUE (DEFAULT, 
+'Gradska apoteka',
+'Bihac',
+'Kralja Tvrtka I 7',
+'037 223-733',
+DEFAULT);
+
+INSERT INTO poslovnice
+VALUE (DEFAULT, 
+'Apoteka Prekounje',
+'Bihac',
+'Bihaćkih Branilaca 9',
+DEFAULT,
+DEFAULT);
+
+INSERT INTO poslovnice
+VALUE (DEFAULT, 
+'Apoteka 9. maj',
+'Goražde',
+'Kulina bana 18',
+'038 221-051',
+DEFAULT);
+
+
+
+
+
